2d8cfad981740f03ad53849ae3405e48e1e8e247
AnnaLevv <anyaa_dms@mail.ru>
2022-09-28 20:14:57 -0700

    hide message

---
+++ src/finalProgect/finalProgect.java
@@ -0,0 +1,11 @@
+package finalProgect;
+
+public class finalProgect {
+
+	public static void main(String[] args) {
+		System.out.println("My husbant is a tiranozzzzavrrrrrr!!!!");
+		System.out.println("Help meeeeee");
+
+	}
+
+}
