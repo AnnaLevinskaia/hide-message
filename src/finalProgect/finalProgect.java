bad0e91d8dbfcf9697942238e3e49997c14e697e
AnnaLevv <anyaa_dms@mail.ru>
2022-09-28 20:52:06 -0700

    second hide message

--- src/finalProgect/finalProgect.java
+++ src/finalProgect/finalProgect.java
@@ -4,7 +4,7 @@ public class finalProgect {
 
 	public static void main(String[] args) {
 		System.out.println("My husbant is a tiranozzzzavrrrrrr!!!!");
-		System.out.println("Help meeeeee");
+	
 
 	}
 
---
+++ src/finalProgect/finalProject1.java
@@ -0,0 +1,10 @@
+package finalProgect;
+
+public class finalProject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Honey byyyyyyy");
+	}
+
+}
